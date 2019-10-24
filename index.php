3e9b81419ffadc3b665cdd17d77601c39b228a37
oppaiyuu <oppaiyuu@gmail.com>
2019-10-24 08:32:09 +0000

    loops

--- index.php
+++ index.php
@@ -1,5 +1,14 @@
+<html>
+<title> JELGAVAS TEHNIKUMS </title>
+<body>
+<h1> Hello? </h1>
+<p><font color="red">Is anyone here?</font></p>
+<img src="https://i.kym-cdn.com/photos/images/newsfeed/001/393/650/27f.jpg" alt="sdjahj" width="500" height="333">
+
+
 <?php
-
-echo "Hello World abuharis testing"
-
+echo "broether, may i have some loops?"
 ?>
+
+</body>
+</html>
